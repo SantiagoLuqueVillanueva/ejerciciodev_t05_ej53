9480db070ef6aafbd9d7ec63ae295aade8db60f9
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-11-16 19:08:22 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int altura = Integer.parseInt(System.console().readLine("Introduzca la altura de la figura: "));
+
+        for (int i = 0; i < altura; i++) {
+            for (int j = i; j < altura; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
